86a6e915062d724c2ed9216df6adfe3d467b03ef
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-20 10:36:30 +0400

    feat: implement getAngleBetweenVectors

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -106,8 +106,15 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  let first = Math.atan(y1 / x1);
+  let second = Math.atan(y2 / x2);
+  if (x1 < 0) first += Math.PI;
+  if (x2 < 0) second += Math.PI;
+  return Math.min(
+    Math.abs(first - second),
+    2 * Math.PI - Math.abs(first - second)
+  );
 }
 
 /**
