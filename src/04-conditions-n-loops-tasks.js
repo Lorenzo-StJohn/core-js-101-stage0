ebca135e89ab90ceaecef9e8e50a8d4c9b74f949
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-20 13:50:10 +0400

    feat: implement doRectanglesOverlap

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -128,8 +128,24 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const y1 =
+    (rect1.top >= rect2.top && rect1.top <= rect2.top + rect2.height) ||
+    (rect1.top + rect1.height >= rect2.top &&
+      rect1.top + rect1.height <= rect2.top + rect2.height);
+  const x1 =
+    (rect1.left >= rect2.left && rect1.left <= rect2.left + rect2.width) ||
+    (rect1.left + rect1.width >= rect2.left &&
+      rect1.left + rect1.width <= rect2.left + rect2.width);
+  const y2 =
+    (rect2.top >= rect1.top && rect2.top <= rect1.top + rect1.height) ||
+    (rect2.top + rect2.height >= rect1.top &&
+      rect2.top + rect2.height <= rect1.top + rect1.height);
+  const x2 =
+    (rect2.left >= rect1.left && rect2.left <= rect1.left + rect1.width) ||
+    (rect2.left + rect2.width >= rect1.left &&
+      rect2.left + rect2.width <= rect1.left + rect1.width);
+  return (x1 && y1) || (x2 && y2);
 }
 
 /**
