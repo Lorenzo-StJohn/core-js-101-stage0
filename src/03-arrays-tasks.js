d7cfc5cf8ae2a3862c8e5bad49a9a78a1304c7cd
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-20 12:57:42 +0400

    feat: implement getFalsyValuesCount

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -363,8 +363,11 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((sumArr, el) => {
+    if (Boolean(el) === false) return sumArr + 1;
+    return sumArr;
+  }, 0);
 }
 
 /**
