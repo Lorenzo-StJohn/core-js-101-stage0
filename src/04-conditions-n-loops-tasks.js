2139d6ae7f4dd7667dc4cac1cf0b6414be90af00
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-20 13:56:44 +0400

    feat: implement getIntervalString

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -226,8 +226,11 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const f = isStartIncluded ? '[' : '(';
+  const s = isEndIncluded ? ']' : ')';
+  if (a > b) return `${f}${b}, ${a}${s}`;
+  return `${f}${a}, ${b}${s}`;
 }
 
 /**
