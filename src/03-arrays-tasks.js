429bff08479668e97d0bcfcef32f9f8e7dd30dd3
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-20 12:52:34 +0400

    feat: implement get3TopItems

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -281,8 +281,9 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  if (arr.length < 2) return arr;
+  return arr.sort((a, b) => b - a).slice(0, Math.min(3, arr.length));
 }
 
 /**
