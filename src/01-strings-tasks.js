9cbcea35b13974cae5da628dedf3266443f2d12d
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-20 10:29:17 +0400

    feat: implement function getCardId

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -276,8 +276,11 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  return (
+    '♣♦♥♠'.indexOf(value[value.length - 1]) * 13 +
+    'A234567890JQK'.indexOf(value[value.length - 2])
+  );
 }
 
 module.exports = {
