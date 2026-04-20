9a71da8b9bfd2f1be31a05552840fadcccc2669a
Lorenzo-StJohn <britishaccent@bk.ru>
2026-04-20 13:37:38 +0400

    feat: implement swapHeadAndTail

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -593,8 +593,15 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const move = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 1) {
+    return arr
+      .slice(move + 1, arr.length)
+      .concat(arr[move])
+      .concat(arr.slice(0, move));
+  }
+  return arr.slice(move, arr.length).concat(arr.slice(0, move));
 }
 
 module.exports = {
